53983afede678f3d6f5e3d2935ab188cfa9c43e8
Nelson Foltz <nelsfoltz@gmail.com>
2022-01-17 15:16:38 -0500

    add: connec to DB

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const app = express();
 import dotenv from 'dotenv';
 dotenv.config();
 
-// middleware
+import connectDB from './db/connect.js';
 
+// middleware
 import notFoundMiddleware from './middleware/not-found.js';
 import errorHandlerMiddleware from './middleware/error-handler.js';
 
 app.get('/', (req, res) => {
-  throw new Error('error');
   res.send('Welcome!');
 });
 
@@ -17,4 +17,15 @@ app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server is listening on port ${port}...`));
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URL);
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
