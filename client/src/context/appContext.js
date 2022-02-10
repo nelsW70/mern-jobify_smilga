319dbd52c1e28897f33ebd8ac1b17a88f86db9ba
Nelson Foltz <nelsfoltz@gmail.com>
2022-02-10 16:21:10 -0500

    add: register user complete

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -1,5 +1,6 @@
 import React, { useReducer, useContext } from 'react';
 import reducer from './reducer';
+import axios from 'axios';
 import {
   CLEAR_ALERT,
   DISPLAY_ALERT,
@@ -16,6 +17,7 @@ const initialState = {
   user: null,
   token: null,
   userLocation: '',
+  jobLocation: '',
 };
 
 const AppContext = React.createContext();
@@ -36,7 +38,33 @@ const AppProvider = ({ children }) => {
   };
 
   const registerUser = async currentUser => {
-    console.log(currentUser);
+    dispatch({ type: REGISTER_USER_BEGIN });
+    try {
+      const response = await axios.post('/api/v1/auth/register', currentUser);
+      console.log(response);
+      const { user, token, location } = response.data;
+      dispatch({
+        type: REGISTER_USER_SUCCESS,
+        payload: {
+          user,
+          token,
+          location,
+        },
+      });
+      // will add later
+      // addUserToLocalStorage({
+      //   user,
+      //   token,
+      //   location,
+      // })
+    } catch (error) {
+      console.log(error.response);
+      dispatch({
+        type: REGISTER_USER_ERROR,
+        payload: { msg: error.response.data.msg },
+      });
+    }
+    clearAlert();
   };
 
   return (
