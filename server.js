b68116db1a381c568344873ab07c8e8b86d77b15
Nelson Foltz <nelsfoltz@gmail.com>
2022-02-10 15:10:05 -0500

    add: proxy setup

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const app = express();
 import dotenv from 'dotenv';
 dotenv.config();
 import 'express-async-errors';
-
 // db and authenticate user
 import connectDB from './db/connect.js';
 
@@ -17,8 +16,12 @@ import errorHandlerMiddleware from './middleware/error-handler.js';
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Welcome!');
+// app.get('/', (req, res) => {
+//   res.json({ msg: 'Welcome' });
+// });
+
+app.get('/api/v1', (req, res) => {
+  res.json({ msg: 'API' });
 });
 
 app.use('/api/v1/auth', authRouter);
