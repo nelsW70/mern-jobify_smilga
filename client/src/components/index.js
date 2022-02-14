a3702275ad0b52872fe55ddb4c5e89ab4f4eb408
Nelson Foltz <nelsfoltz@gmail.com>
2022-02-14 09:00:41 -0500

    add: shared layout structure and react icons package added

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,5 +1,8 @@
 import Alert from './Alert';
 import FormRow from './FormRow';
 import Logo from './Logo';
+import Navbar from './Navbar';
+import BigSidebar from './BigSidebar';
+import SmallSidebar from './SmallSidebar';
 
-export { Alert, FormRow, Logo };
+export { Alert, FormRow, Logo, Navbar, BigSidebar, SmallSidebar };
