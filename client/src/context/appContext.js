92c75f319f76b8bd5a75c7dd7db4998c483d08d3
Nelson Foltz <nelsfoltz@gmail.com>
2022-02-21 11:27:24 -0500

    add: axios interceptors

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -31,6 +31,35 @@ const AppContext = React.createContext();
 const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
+  // axios
+  const authFetch = axios.create({
+    baseURL: '/api/v1',
+  });
+
+  // request interceptor
+  authFetch.interceptors.request.use(
+    config => {
+      config.headers.common['Authorization'] = `Bearer ${state.token}`;
+      return config;
+    },
+    error => {
+      return Promise.reject(error);
+    }
+  );
+  // response interceptor
+  authFetch.interceptors.response.use(
+    response => {
+      return response;
+    },
+    error => {
+      console.log(error.response);
+      if (error.response.status === 401) {
+        console.log('AUTH ERROR');
+      }
+      return Promise.reject(error);
+    }
+  );
+
   const displayAlert = () => {
     dispatch({ type: DISPLAY_ALERT });
     clearAlert();
@@ -89,7 +118,12 @@ const AppProvider = ({ children }) => {
   };
 
   const updateUser = async currentUser => {
-    console.log(currentUser);
+    try {
+      const { data } = await authFetch.patch('/auth/updateUser', currentUser);
+      console.log(data);
+    } catch (error) {
+      // console.log(error.response);
+    }
   };
 
   return (
