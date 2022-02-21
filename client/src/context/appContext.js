09c5db715f8aa1b583dff400c69ec75833d20087
Nelson Foltz <nelsfoltz@gmail.com>
2022-02-21 13:39:51 -0500

    add: update user and logout user updates

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -9,6 +9,9 @@ import {
   SETUP_USER_ERROR,
   TOGGLE_SIDEBAR,
   LOGOUT_USER,
+  UPDATE_USER_BEGIN,
+  UPDATE_USER_SUCCESS,
+  UPDATE_USER_ERROR,
 } from './actions';
 
 const token = localStorage.getItem('token');
@@ -46,15 +49,15 @@ const AppProvider = ({ children }) => {
       return Promise.reject(error);
     }
   );
+
   // response interceptor
   authFetch.interceptors.response.use(
     response => {
       return response;
     },
     error => {
-      console.log(error.response);
       if (error.response.status === 401) {
-        console.log('AUTH ERROR');
+        logoutUser();
       }
       return Promise.reject(error);
     }
@@ -118,12 +121,28 @@ const AppProvider = ({ children }) => {
   };
 
   const updateUser = async currentUser => {
+    dispatch({ type: UPDATE_USER_BEGIN });
     try {
       const { data } = await authFetch.patch('/auth/updateUser', currentUser);
-      console.log(data);
+
+      // no token
+      const { user, location, token } = data;
+
+      dispatch({
+        type: UPDATE_USER_SUCCESS,
+        payload: { user, location, token },
+      });
+
+      addUserToLocalStorage({ user, location, token });
     } catch (error) {
-      // console.log(error.response);
+      if (error.response.status !== 401) {
+        dispatch({
+          type: UPDATE_USER_ERROR,
+          payload: { msg: error.response.data.msg },
+        });
+      }
     }
+    clearAlert();
   };
 
   return (
