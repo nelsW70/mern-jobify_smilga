0ae28f2512100c1406a8a7a2997d88897ea703a9
Nelson Foltz <nelsfoltz@gmail.com>
2022-02-27 08:34:36 -0500

    add: charts setup

--- client/src/components/ChartsContainer.jsx
+++ client/src/components/ChartsContainer.jsx
@@ -1,9 +1,25 @@
-const ChartsContainer = () => {
-  return (
-    <div>
-      <h1>charts container</h1>
-    </div>
-  );
-};
+import React, { useState } from 'react';
+import BarChartComponent from './BarChart';
+import AreaChartComponent from './AreaChart';
+import { useAppContext } from '../context/appContext';
+import Wrapper from '../assets/wrappers/ChartsContainer';
 
-export default ChartsContainer;
+export default function ChartsContainer() {
+  const [barChart, setBarChart] = useState(true);
+  const { monthlyApplications: data } = useAppContext();
+
+  return (
+    <Wrapper>
+      <h4>Monthly Applications</h4>
+
+      <button type="button" onClick={() => setBarChart(!barChart)}>
+        {barChart ? 'Area Chart' : 'Bar Chart'}
+      </button>
+      {barChart ? (
+        <BarChartComponent data={data} />
+      ) : (
+        <AreaChartComponent data={data} />
+      )}
+    </Wrapper>
+  );
+}
